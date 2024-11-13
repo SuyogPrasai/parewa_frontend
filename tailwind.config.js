6f62d465f90a000bdd0fbe7bee9fb3f64c7b9fe1
suyog prasai <suyog224356@gmail.com>
2024-11-13 07:51:31 +0000

    added and configured a responsive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['Chakra Petch', 'sans-serif'], // replace 'CustomFont' with your font name
+      },
+      colors: {
+        primary: '#243642',          // Main primary brand color (Dark Blue)
+        accent: 'blue-500',           // Accent color for highlights (Light Blue)
+        success: '#629584',          // Success or positive state (Greenish)
+        background: '#E2F1E7',       // Light background color (Soft Greenish)
+        darkBackground: '#00001C',   // Dark background color (Dark Navy/Black)
+        buttonBackground: "#eaeaea", // Light background color for buttons
+        textPrimary: "#333333",      // Main text color (Dark Gray)
+        textSecondary: "#666666",    // Secondary text color (Gray)
+        border: "#dddddd",           // Border color (Light Gray)
+      },
+    },
   },
   plugins: [],
 }
