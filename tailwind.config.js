13701a5c3a9c1b36ff07197260f5aa5c12f2e01c
suyog prasai <suyog224356@gmail.com>
2024-11-13 07:33:07 +0000

    configured tailwind and it works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,11 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        title: ['Chakra Petch', 'sans-serif'], // Custom font for the title
-      },
-      colors: {
-        // Primary colors
-        primary: '#243642',             // Dark Slate
-        highlight: '#6095ba',           // Soft Blue
-        accentGreen: '#629584',         // Muted Green
-        background: '#E2F1E7',          // Light Mint Background
-        darkBlue: '#00001C',            // Almost Black Blue
-        buttonBackground: '#eaeaea',    // Light Gray for Buttons
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
